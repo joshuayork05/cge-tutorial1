d2beb32af9fd569b2b5f9016c797d95baabc368c
Josh <y010613n@student.staffs.ac.uk>
2024-01-31 17:10:13 +0000

    Reimplemented looping puzzle

--- cge assignment 1/Assets/Scripts/MazeMaster.cs
+++ cge assignment 1/Assets/Scripts/MazeMaster.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class MazeMaster : MonoBehaviour
 {
+    [SerializeField] public TopDownCharacterController player_info;
+
     private int exit_num;
     private int cor_exit_num;
 
     //Setting the defaults for progression phases in the areas
-    private bool phase1 = false;
+    private bool phase1 = true;
     private bool phase2 = false;
+    private bool phase3 = false;
 
+    Vector3 mazeStart;
+
+    private void Start()
+    {
+        UpdateExitValues();
+    }
+
+    //gets and stores the exit number of any exit collided with
     public void StoreExitNum(int exitnum)
     {
         exit_num = exitnum;
@@ -23,42 +34,73 @@ public class MazeMaster : MonoBehaviour
         if (exit_num == cor_exit_num)
         {
             Debug.Log("Correct exit");
+            UpdateCurrentPhase(true);
         }
         else
         {
             Debug.Log("Wrong exit");
+            UpdateCurrentPhase(false);
         }
     }
 
-    private void UpdatePhase(int current_phasenum, bool moveon)
+    private void UpdateCurrentPhase(bool correct_exit)
     {
-        if (current_phasenum == 0)
+        if (correct_exit)
         {
-            phase1 = !phase1;
+            if (phase1 == true)
+            {
+                phase1 = false;
+                phase2 = true;
+            }
+            else if (phase2 == true)
+            {
+                phase2 = false;
+                phase3 = true;
+            }
+            else if (phase3 == true)
+            {
+                EndOfMaze();
+            }
+
+            UpdateExitValues();
+
         }
-        else if (current_phasenum == 1)
+        else
         {
-            phase2 = !phase2;
-        }
-        else if (phase2 == true && moveon == true)
-        {
-            Debug.Log("Finished");
+            ResetMaze();
         }
     }
 
-    private void Update()
+    //updates the correct exit
+    private void UpdateExitValues()
     {
-        if (phase1 == false && phase2 == false)
+        if (phase1 == true)
         {
             cor_exit_num = 1;
-        }
-        else if (phase1 == true)
-        {
-            cor_exit_num = 2;
         }
         else if (phase2 == true)
         {
             cor_exit_num = 3;
         }
+        else if (phase3 == true)
+        {
+            cor_exit_num = 2;
+        }
+    }
+
+    private void ResetMaze()
+    {
+        mazeStart = new Vector3(-11.5f, 5f, 0f);
+
+        phase1 = true;
+        phase2 = false;
+        phase3 = false;
+        UpdateExitValues();
+        player_info.transform.position = mazeStart;
+    }
+
+    private void EndOfMaze()
+    {
+        Debug.Log("You have successfully escaped!");
     }
 }
